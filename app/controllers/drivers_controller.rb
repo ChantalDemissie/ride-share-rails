413b9e51937a443935fae385b7fa5edd8e53d498
Chantal Demissie <chantaldemissie@yahoo.com>
2019-04-16 16:32:44 -0700

    created skeleton for methods

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,30 @@
 class DriversController < ApplicationController
+
+  #def add_passenger
+  def create
+    #error validation required
+  end
+
+  #def list_passenger
+  def index
+  end
+
+  #def show_earnings
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+    #error validation required
+  end
+
+  #add new trip for specific passenger
+  def new
+    #select driver automatically
+    #starts with no rating
+  end
+
+
 end
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,27 @@
 class PassengersController < ApplicationController
+
+#def add_passenger
+def create
+  #error validation required
+end
+
+#def list_passenger
+def index
+end
+
+#def show_passenger
+def show
+end
+
+def edit
+end
+
+def delete
+  #error validation required
+end
+
+#add new trip for specific passenger
+def new
+  #select driver automatically
+  #starts with no rating
 end
