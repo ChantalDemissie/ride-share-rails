3d491eab09e0812bbc1914688aed9737d5460850
ranuseh <rosalyn.anuseh@gmail.com>
2019-04-16 17:48:35 -0700

    Added passenger list method and page rendered

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,27 +1,30 @@
+# frozen_string_literal: true
+
 class PassengersController < ApplicationController
+  # def add_passenger
+  def create
+    # error validation required
+  end
 
-#def add_passenger
-def create
-  #error validation required
-end
+  # def list_passenger
+  def index
+    @passengers = Passenger.all.sort_by(&:id)
+  end
 
-#def list_passenger
-def index
-end
+  # def show_passenger
+  def show
+    @passenger = passenger.all
+  end
 
-#def show_passenger
-def show
-end
+  def edit; end
 
-def edit
-end
+  def delete
+    # error validation required
+  end
 
-def delete
-  #error validation required
-end
-
-#add new trip for specific passenger
-def new
-  #select driver automatically
-  #starts with no rating
+  # add new trip for specific passenger
+  def new
+    # select driver automatically
+    # starts with no rating
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   root 'trips#index'
 
-  # resources :trips
-  # resources :drivers
-  # resources :passengers
+  resources :trips
+  resources :drivers
+  resources :passengers
 
-  get '/trips', to: 'trips#index', as: 'trips'
 end
